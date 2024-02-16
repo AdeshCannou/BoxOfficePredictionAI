858f4f6ad0d3a2de99d0bb05eca28a477c48645c
Adesh Cannou <66022289+AdeshCannou@users.noreply.github.com>
2024-02-16 21:44:38 +0100

    bug commit api

--- app.py
+++ app.py
@@ -1,45 +1,52 @@
 from flask import Flask, request, jsonify
 import joblib
-import pandas as pd
 
 app = Flask(__name__)
 
-# Charger le modèle et le scaler
-model = joblib.load('ridge_regression_model_alpha_100.joblib')
-scaler = joblib.load('scaler.joblib')
-mlb = joblib.load('mlb.joblib')
+# Charger le modèle entraîné depuis le fichier pickle
+ridge_model = joblib.load('linear_model.pkl')
 
-# Extraire les noms de colonnes à partir du MultiLabelBinarizer
-mlb_feature_names = mlb.classes_
+# Lire les colonnes depuis le fichier colonnes.txt
+with open('columns.txt', 'r') as file:
+    expected_columns = file.read().splitlines()
+
+def create_columns(test_data):
+    columns = []
+    for feature, values in test_data.items():
+        if isinstance(values, list):
+            for value in values:
+                columns.append(f"{feature}_{value}")
+        elif feature not in ['runtime', 'budget_adj']:
+            columns.append(f"{feature}_{values}")
+    return columns
+
+def preprocess_test_data(test_data, expected_columns):
+    test_array = []
+    generated_columns = create_columns(test_data)
+    for col in expected_columns:
+        if col in generated_columns:
+            test_array.append(1)
+        elif col == 'runtime':
+            test_array.append(test_data['runtime'])
+        elif col == 'budget_adj':
+            test_array.append(test_data['budget_adj'])
+        else:
+            test_array.append(0)
+    return test_array
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # Récupérer les données envoyées en POST
     json_data = request.get_json()
 
-    # Créer un DataFrame à partir des données
-    input_data = pd.DataFrame(json_data, index=[0])
+    # Prétraiter les données de test
+    test_array = preprocess_test_data(json_data, expected_columns)
 
-    # Vérifier que les noms de colonnes sont cohérents avec ceux utilisés lors de l'entraînement
-    if not set(input_data.columns) == set(mlb_feature_names):
-        missing_features = set(mlb_feature_names) - set(input_data.columns)
-        return jsonify({'error': f'Missing features: {missing_features}'}), 400
+    # Faire des prédictions sur les données de test
+    predicted_revenue = ridge_model.predict([test_array])
 
-    # Prétraiter les données avec le MultiLabelBinarizer
-    for col in ['cast', 'director', 'genres', 'production_companies']:
-        input_data[col] = input_data[col].apply(lambda x: [x])  # Convertir chaque valeur en liste pour correspondre au format attendu par le mlb
-        input_data[col] = mlb.transform(input_data[col])
-
-    # Normaliser les données
-    input_data_scaled = scaler.transform(input_data)
-
-    # Faire la prédiction
-    prediction = model.predict(input_data_scaled)
-
-    # Formater la réponse
-    response = {'prediction': prediction[0]}
-
-    return jsonify(response)
+    # Retourner les prédictions au format JSON
+    return jsonify({'predicted_revenue': predicted_revenue[0]})
 
 if __name__ == '__main__':
     app.run(debug=True)
